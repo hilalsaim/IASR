0849ab8f7da96e86c7bfdeb099c32ec89e400416
hilalsaim <hilalsaim@gmail.com>
2021-01-24 23:45:09 +0100

    update

---
+++ src/camera.py
@@ -0,0 +1,21 @@
+import cv2
+
+from src import imageFunctions
+
+def capture():
+    vc = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    if vc.isOpened():
+        _, frame = vc.read()
+        return imageFunctions.Image(frame)
+
+
+def openCamera():
+    webcamera = cv2.VideoCapture(0)
+    success = True
+
+    face_detector = imageFunctions.FaceDetector()
+
+    while success:
+        success, frame = webcamera.read()
+        face_detector.show(imageFunctions.Image(frame), wait=False)
+        cv2.waitKey(20)
